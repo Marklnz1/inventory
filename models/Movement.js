90d35c7e7f3bef3fa0413d8e32f44e2488120ba0
Marklnz1 <jhon4lnz@gmail.com>
2024-12-07 17:20:56 -0500

    asdasdd

--- models/Movement.js
+++ models/Movement.js
@@ -2,14 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const MovementSchema = new Schema(
-  {
+  generateFields({
     product: { type: String },
-    code: { type: Number, required: true },
-    uuid: { type: String, required: true, unique: true },
     quantity: Number,
     price: Number,
-    state: String,
-  },
+  }),
   { timestamps: true }
 );
 const Movement = mongoose.model("movement", MovementSchema, "movement");
